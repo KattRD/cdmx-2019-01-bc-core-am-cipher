c7c970ca6903b6a5b6529809cba6a38eaf0eafcc
KattRD <kat.rod.dom@gmail.com>
2019-01-15 20:41:11 -0600

    Creando primeras estructuras y propuesta de funciones respuesta

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,32 @@
 window.cipher = {
-  // ... 
-};
+  function cipherText() {
+  const textC = "";
+  for (i = 0; i<text.length; i++) {
+    const ubication =(text.CharCodeAt(i)+ (document.getElementById("offs"));
+    const textCiph =String.fromCharCode(ubication);
+    textC+=textCiph;
+  }
+return textC;
+}
+function decipherText() {
+  const textD = "";
+  for (j = 0; j<textC.length; j++) {
+    const newUbication1 =(textC.CharCodeAt(j)+ (document.getElementById("offs"));
+    const textDeciph =String.fromCharCode(newUbication);
+    textD+=textDeciph;
+  }
+return textD;
+}
+
+
+
+
+
+
+
+//function cipherText() {
+//cipher.encode(offset, string);
+//function decipherText() {
+// cipher.decode(offset, string)
+
+
