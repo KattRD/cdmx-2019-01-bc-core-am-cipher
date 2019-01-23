f4743310ae4df87ac1dbcdd3051c3cbb527c1024
KattRD <kat.rod.dom@gmail.com>
2019-01-23 15:16:03 -0600

    Agregando más elemntos CSS y DOM

--- src/cipher.js
+++ src/cipher.js
@@ -3,14 +3,9 @@ window.cipher = {
 
     let mensajeC=""; 
     let i= 0;
-    //console.log(mensaje.length)
     for (i = 0; i<mensaje.length; i++) {
-    // Capturo la letra
     let letraCapturada = mensaje[i];
-    // Pasar letra a Mayúsculas
     let letraMayuscula = letraCapturada.toUpperCase();
-    //console.log("letraMayuscula: ", letraMayuscula)
-    // Obtener posición en el código ASCII
     let converterAscci=letraMayuscula.charCodeAt(0);
     let newUbication=(converterAscci - 65 + parseInt(offSet)) % 26 + 65;
     let newMensaje=String.fromCharCode(newUbication);
@@ -23,22 +18,13 @@ window.cipher = {
 
     let mensajeC2=""; 
     let j= 0;
-    //console.log(mensaje.length)
     for (j = 0; j<mensaje2.length; j++) {
-    // Capturo la letra
     let letraCapturada2 = mensaje2[j];
-    // Pasar letra a Mayúsculas
     let letraMayuscula2 = letraCapturada2.toUpperCase();
-    //console.log(letraMayuscula);
-    // Obtener posición en el código ASCII
-    let converterAscci2=letraMayuscula2.charCodeAt(0);
-    //console.log(converterAscci); 
+    let converterAscci2=letraMayuscula2.charCodeAt(0); 
     let newUbication2=(converterAscci2 + 65 - parseInt(offSet2)) %26+65;
-    //console.log(newUbication);
     let newMensaje2=String.fromCharCode(newUbication2);
-    //console.log(newMensaje);
     mensajeC2+= newMensaje2;
-    //console.log(mensajeC);
     }
 
       return mensajeC2;
